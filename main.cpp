fb3a6ff31bd66f2d47b161228d1a92eaf13042c9
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-03-22 20:53:05 +0000

    Adding Float Delay
    Signed-off-by: Philipleahy <philip.leahy@HOTMAIL.COM>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
 #include "mbed.h"
 
+//Embedded Systems Lab 1
+
 Serial pc(USBTX, USBRX);
-InterruptIn Down(p12);
-InterruptIn Left(p13);
-InterruptIn Center(p14);
-InterruptIn Up(p15);
-InterruptIn Right(p16);
+InterruptIn Down(p12);//Move Joystick Down
+InterruptIn Left(p13);//Move Joystick Left
+InterruptIn Center(p14);//Press Joystick Center Button
+InterruptIn Up(p15);//Move Joystick Up
+InterruptIn Right(p16);//Move Joystick Right
+float delay(0.25);//Variable in ms. Started at 0.1 and found correct result at 0.25
 
 void down()
 {
     printf("Joystick Down \r\n");
+    wait(delay);
 }
 void left()
 {
     printf("Joystick Left \r\n");
+    wait(delay);
 }
 void centre()
 {
     printf("Joystick Center \r\n");
+    wait(delay);
 }
 void up()
 {
     printf("Joystick Up \r\n");
+    wait(delay);
 }
 void right()
 {
     printf("Joystick Right \r\n");
+    wait(delay);
 }
 int main() 
 {
